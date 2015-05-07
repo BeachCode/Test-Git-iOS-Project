bc35495f277b35d4e3474278c3a9dc42a703ac44
Dustin M <Eleuthera@Dustins-Air.T-mobile.com>
2015-05-07 19:45:12 -0400

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Dustin M on 5/7/15.
+//  Copyright (c) 2015 Vento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
