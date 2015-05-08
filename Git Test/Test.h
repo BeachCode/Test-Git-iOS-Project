876c455f2fb1533f2bd7e649533c2154d8f4d669
Dustin M <Eleuthera@Dustins-Air.T-mobile.com>
2015-05-07 20:24:14 -0400

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Dustin M on 5/7/15.
+//  Copyright (c) 2015 Vento. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Dustin M on 5/7/15.
+//  Copyright (c) 2015 Vento. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
